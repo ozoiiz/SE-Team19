e7221ec86aa01334e5be5720f44f1557629385c9
jieuni52 <129067367+jieuni52@users.noreply.github.com>
2025-05-09 15:35:32 +0900

    Update GameView.java

--- GameView.java
+++ GameView.java
@@ -1,5 +1,7 @@
 package com.example.yutgame;
 
 public interface GameView {
-
+    void updateBoard(Game game);
+    void showMessage(String message);
+    void setController(GameController controller);
 }
