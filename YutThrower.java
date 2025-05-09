c61c09dee67e436ac729bc87bfbe3d18db548c90
jieuni52 <129067367+jieuni52@users.noreply.github.com>
2025-05-09 15:37:20 +0900

    Update YutThrower.java

--- YutThrower.java
+++ YutThrower.java
@@ -1,5 +1,21 @@
 package com.example.yutgame;
 
-public class YutThrower {
+import java.util.Random;
 
+public class YutThrower {
+    private Random rand;
+
+    public YutThrower() {
+        rand = new Random();
+    }
+
+    public YutResult throwYut() {
+        int r = rand.nextInt(16); // 0부터 15까지
+        if (r == 0) return YutResult.BACKDO;
+        else if (r < 4) return YutResult.DO;
+        else if (r < 10) return YutResult.GAE;
+        else if (r < 14) return YutResult.GEOL;
+        else if (r == 14) return YutResult.YUT;
+        else return YutResult.MO;
+    }
 }
