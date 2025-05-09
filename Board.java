2098edd9934fc1ae1957dd6dca4ac207e592d821
ozoiiz <129177234+ozoiiz@users.noreply.github.com>
2025-05-09 15:03:10 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,4 +1,23 @@
 package com.example.yutgame;
 
 public class Board {
+    private BoardShape shape;
+    private int totalCells;
+
+    public Board(BoardShape shape) {
+        this.shape = shape;
+        if(shape == BoardShape.SQUARE) {
+            totalCells = 29;
+        } else if(shape == BoardShape.PENTAGON) {
+            totalCells = 36;
+        } else if(shape == BoardShape.HEXAGON) {
+            totalCells = 43;
+        }
     }
+    public BoardShape getShape() {
+        return shape;
+    }
+    public int getTotalCells() {
+        return totalCells;
+    }
+}
